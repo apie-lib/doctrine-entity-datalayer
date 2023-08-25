655cf36524c17aee0e5244d5644dac340e0d9532
pjordaan <pjordaan@users.noreply.github.com>
2023-08-25 20:38:06 +0000

    Update file(s) "packages/doctrine-entity-datalayer/." from "apie-lib/apie-lib-monorepo"

--- src/OrmBuilder.php
+++ src/OrmBuilder.php
@@ -7,8 +7,10 @@ use Apie\Core\Persistence\Lists\PersistenceFieldList;
 use Apie\Core\Persistence\Metadata\EntityMetadata;
 use Apie\DoctrineEntityConverter\Interfaces\GeneratedDoctrineEntityInterface;
 use Apie\DoctrineEntityConverter\OrmBuilder as DoctrineEntityConverterOrmBuilder;
+use Apie\DoctrineEntityDatalayer\Exceptions\CouldNotUpdateDatabaseAutomatically;
 use Doctrine\Common\EventManager;
 use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\Exception\DriverException;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\ORMSetup;
@@ -44,17 +46,25 @@ class OrmBuilder
         return 'Generated\\';
     }
 
-    protected function runMigrations(EntityManagerInterface $entityManager): void
+    protected function runMigrations(EntityManagerInterface $entityManager, bool $firstCall = true): void
     {
         $tool = new SchemaTool($entityManager);
         $classes = $entityManager->getMetadataFactory()->getAllMetadata();
-        $sql = $tool->getDropDatabaseSQL();
-        foreach ($sql as $statement) {
-            $entityManager->getConnection()->exec($statement);
-        }
-        $sql = $tool->getUpdateSchemaSql($classes);
-        foreach ($sql as $statement) {
-            $entityManager->getConnection()->exec($statement);
+        
+        try {
+            $sql = $tool->getUpdateSchemaSql($classes);
+            foreach ($sql as $statement) {
+                $entityManager->getConnection()->exec($statement);
+            }
+        } catch (DriverException $driverException) {
+            if ($firstCall) {
+                $sql = $tool->getDropDatabaseSQL();
+                foreach ($sql as $statement) {
+                    $entityManager->getConnection()->exec($statement);
+                }
+                $this->runMigrations($entityManager, false);
+            }
+            throw new CouldNotUpdateDatabaseAutomatically($driverException);
         }
     }
 
