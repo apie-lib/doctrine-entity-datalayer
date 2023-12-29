ef0bce8442245b4b08f27ccf6b6f40f7d0d76799
pjordaan <pjordaan@users.noreply.github.com>
2023-12-29 05:10:37 +0000

    Update file(s) "packages/doctrine-entity-datalayer/." from "apie-lib/apie-lib-monorepo"

--- src/EntityReindexer.php
+++ src/EntityReindexer.php
@@ -69,7 +69,7 @@ final class EntityReindexer
         $connection = $entityManager->getConnection();
         $query = sprintf(
             'UPDATE %s AS t
-            SET idf = %s((%s)/(%s = t.text))
+            SET idf = IFNULL(%s((%s)/(%s = t.text)), 0)
             WHERE EXISTS (SELECT 1 FROM (SELECT text, COUNT(DISTINCT %s) AS documents_with_term FROM %s GROUP BY text) AS sub WHERE sub.text = t.text);',
             $tableName,
             $connection->getDatabasePlatform() instanceof SqlitePlatform ? '' : 'log',
