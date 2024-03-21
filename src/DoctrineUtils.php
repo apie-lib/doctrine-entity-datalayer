1d052c4f01f826ea0716edbd22b9618cb08445e0
pjordaan <pjordaan@users.noreply.github.com>
2024-03-21 15:23:51 +0000

    Update file(s) "packages/doctrine-entity-datalayer/." from "apie-lib/apie-lib-monorepo"

--- src/DoctrineUtils.php
+++ src/DoctrineUtils.php
@@ -1,6 +1,8 @@
 <?php
 namespace Apie\DoctrineEntityDatalayer;
 
+use Apie\StorageMetadata\Attributes\GetSearchIndexAttribute;
+use Apie\StorageMetadata\Attributes\OneToManyAttribute;
 use Doctrine\Persistence\Proxy;
 use ReflectionClass;
 use ReflectionProperty;
@@ -13,6 +15,16 @@ final class DoctrineUtils
     private array $visited = [];
     private function __construct()
     {
+    }
+
+    private function hasOneToManyWithProperty(ReflectionProperty $property): bool
+    {
+        foreach ($property->getAttributes(OneToManyAttribute::class) as $oneToManyAttribute) {
+            if ($oneToManyAttribute->newInstance()->propertyName === null) {
+                return false;
+            }
+        }
+        return true;
     }
 
     private function loadAll(object $entity): void
@@ -29,6 +41,12 @@ final class DoctrineUtils
             if ($property->isStatic() || !$property->isInitialized($entity)) {
                 continue;
             }
+            $attributes = $property->getAttributes(GetSearchIndexAttribute::class);
+            if (!empty($attributes) || !$this->hasOneToManyWithProperty($property)) {
+                continue;
+            }
+            
+
             $propertyValue = $property->getValue($entity);
             if (is_object($propertyValue)) {
                 $this->loadAll($propertyValue);
