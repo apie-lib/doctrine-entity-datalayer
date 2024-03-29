0dc2223551e9b78236c0d0e0c359c3283865c53b
pjordaan <pjordaan@users.noreply.github.com>
2024-03-29 16:10:16 +0000

    Update file(s) "packages/doctrine-entity-datalayer/." from "apie-lib/apie-lib-monorepo"

--- src/Query/RequiresPermissionFilter.php
+++ src/Query/RequiresPermissionFilter.php
@@ -2,13 +2,13 @@
 namespace Apie\DoctrineEntityDatalayer\Query;
 
 use Apie\Common\ContextConstants;
-use Apie\Core\Permissions\PermissionInterface;
-use Apie\Core\Permissions\RequiresPermissionsInterface;
 use Apie\Core\Attributes\LoggedIn;
 use Apie\Core\BoundedContext\BoundedContextId;
-use Apie\Core\Entities\EntityInterface;
 use Apie\Core\Datalayers\Search\QuerySearch;
+use Apie\Core\Entities\EntityInterface;
 use Apie\Core\IdentifierUtils;
+use Apie\Core\Permissions\PermissionInterface;
+use Apie\Core\Permissions\RequiresPermissionsInterface;
 use Apie\DoctrineEntityDatalayer\Enums\SortingOrder;
 use Doctrine\DBAL\Connection;
 use ReflectionClass;
